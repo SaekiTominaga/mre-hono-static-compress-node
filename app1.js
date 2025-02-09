643247349b0e1021e2b57830d1e04336e153f8b1
SaekiTominaga <kcsu3470+github@gmail.com>
2025-02-09 14:10:03 +0900

    MRE code commit

---
+++ app1.js
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+const app = new Hono();
+
+app.use(
+  serveStatic({
+    root: "static",
+    precompressed: true,
+    onFound: (localPath, context) => {},
+  })
+);
+
+if (process.env["TEST"] !== "test") {
+  serve({
+    fetch: app.fetch,
+  });
+  console.info(`Server is running on http://localhost:3000`);
+}
+
+export default app;
